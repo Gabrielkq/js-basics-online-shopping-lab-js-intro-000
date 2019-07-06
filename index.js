4337313624d1a62d85b2705c9678b04cab1d96ec
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-07-06 03:17:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,9 +53,9 @@ function removeFromCart(item) {
     cart.splice(i, 1);
     return cart;
   }
-    return "That item is not in your cart.";
+    
  
-}}
+}return "That item is not in your cart.";}
 
 function placeOrder(cardNumber) {
  if (cardNumber===undefined){ 
